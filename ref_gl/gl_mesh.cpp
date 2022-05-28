b0f949902f9bd58dfad59260ef1dbdade3588394
muxyrmuc <muxyrmuc@gmail.com>
2022-05-29 02:16:26 +0300

    gl_mesh.cpp

--- ref_gl/gl_mesh.cpp
+++ ref_gl/gl_mesh.cpp
@@ -445,10 +445,10 @@ static qboolean R_CullAliasModel(vec3_t bbox[8], entity_t* e) {
         }
 
         if (aggregatemask) {
-            return true;
+            return kTrue;
         }
 
-        return false;
+        return kFalse;
     }
 }
 
