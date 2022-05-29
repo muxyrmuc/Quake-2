f4a39cc6a1f122794b975e6d6bd3d5b453071f9f
muxyrmuc <muxyrmuc@gmail.com>
2022-05-29 04:14:12 +0300

    net_wins.cpp

--- win32/net_wins.cpp
+++ win32/net_wins.cpp
@@ -91,7 +91,7 @@ qboolean NET_CompareAdr(netadr_t a, netadr_t b) {
         return kFalse;
 
     if (a.type == NA_LOOPBACK)
-        return TRUE;
+        return kTrue;
 
     if (a.type == NA_IP) {
         if (a.ip[0] == b.ip[0] && a.ip[1] == b.ip[1] && a.ip[2] == b.ip[2] && a.ip[3] == b.ip[3] && a.port == b.port)
@@ -118,7 +118,7 @@ qboolean NET_CompareBaseAdr(netadr_t a, netadr_t b) {
         return kFalse;
 
     if (a.type == NA_LOOPBACK)
-        return TRUE;
+        return kTrue;
 
     if (a.type == NA_IP) {
         if (a.ip[0] == b.ip[0] && a.ip[1] == b.ip[1] && a.ip[2] == b.ip[2] && a.ip[3] == b.ip[3])
