bdb706542364e84c9ecd73e545778b7c61408c97
Mikhail Morozov <muxyrmuc@gmail.com>
2024-06-08 10:02:16 +0300

    fix braces, const

--- game/g_local.h
+++ game/g_local.h
@@ -579,7 +579,7 @@ void PrecacheItem(gitem_t* it);
 void InitItems(void);
 void SetItemNames(void);
 gitem_t* FindItem(const char* pickup_name);
-gitem_t* FindItemByClassname(char* classname);
+gitem_t* FindItemByClassname(const char* classname);
 #define ITEM_INDEX(x) ((x)-itemlist)
 edict_t* Drop_Item(edict_t* ent, gitem_t* item);
 void SetRespawn(edict_t* ent, float delay);
